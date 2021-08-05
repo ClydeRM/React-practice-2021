889fb307ae91caf9fcdcd72ea2667772eebed5c4
ClydeRM <clyde.official@icloud.com>
2021-08-05 21:51:35 +0800

    globa deleteTask func and inport Head and Task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import Header from "./components/Header";
+import { useState } from 'react'
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+
 
 function App() {
-  return (
-    <div className="container">
-        <Header />
-    </div>
-  );
+
+	const [tasks, setTasks] = useState(
+		[
+			{
+				"id": 1,
+				"text": "Doctors Appointment",
+				"day": "Feb 5th at 2:30pm",
+				"reminder": true
+			},
+			{
+				"id": 2,
+				"text": "Meeting at School",
+				"day": "Feb 6th at 1:30pm",
+				"reminder": true
+			}
+		]
+	)
+
+	// Delete Task
+	const deleteTask = (id) => {
+		// console.log('Delete', id)
+		setTasks(tasks.filter((task) => task.id !== id));
+	}
+
+
+	return (
+		<div className="container">
+			{/* <Header title='Hello' />  override default props.title */}
+			{/* <Header title={'Task Tracker'}/>  using PropTypes expected var type*/}
+			<Header />
+			{
+				tasks.length > 0
+					? <Tasks tasks={tasks} onDelete={deleteTask} />
+					: 'NO Task'
+			}
+		</div>
+	);
 }
+
 
 export default App;
