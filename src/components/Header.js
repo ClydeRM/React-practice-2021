b857c6b57d6109e932842f2bcfc2ffd18d832819
ClydeRM <clyde.official@icloud.com>
2021-08-05 21:38:10 +0800

    complete Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,33 @@
-const Header = () => {
+import PropTypes from 'prop-types' // impt+table auto create
+import Button from './Button'
+
+const Header = ({ title }) => {
+
+    const onClick = (e) =>{
+        console.log(e)
+    }
+
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className='header'>
+            {/* <h1 style={ headingStyle }>{title}</h1> Can using stylesheet but is camoCase */}
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
 
-export default Header
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
 
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+
+export default Header
