308d7e73acb12dcc5358e9388d60d76d23dce750
ClydeRM <clyde.official@icloud.com>
2021-08-06 19:26:01 +0800

    Dynamic change show  use uselocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
 import PropTypes from 'prop-types' // impt+table auto create
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
 
-    const onClick = (e) =>{
-        console.log(e)
-    }
+    const location = useLocation()
 
     return (
         <header className='header'>
             {/* <h1 style={ headingStyle }>{title}</h1> Can using stylesheet but is camoCase */}
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
+            {location.pathname === '/' && (
+                <Button 
+                    color={showAdd ? 'red' : 'green' }
+                    text={showAdd ? 'Close' : 'Add' }
+                    onClick={onAdd} 
+                />
+            )}
         </header>
     )
 }
